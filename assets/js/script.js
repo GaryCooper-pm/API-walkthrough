1bd0377fed21af909e44a1b7d26228c5660691bc
GaryCooper-pm <gary@pixelmixel.co.uk>
2022-06-11 09:16:05 +0000

    Add script to return API_KEY expirty date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-Yj2xLA2snV5xPgf6RlwF0v8sZI4
+const API_KEY = "Yj2xLA2snV5xPgf6RlwF0v8sZI4";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        console.log(data.expiry);
+    }
+}
